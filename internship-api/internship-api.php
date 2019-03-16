e85d8194f826ca45f4c562dcf3914629e0bf808e
Anton Levholm <anton.badminton@gmail.com>
2019-03-16 10:36:38 +0100

    Cache request data

--- internship-api/internship-api.php
+++ internship-api/internship-api.php
@@ -11,9 +11,8 @@ Author URI: levholm.se
 https://sites.google.com/iaeste.org/ep-user-guide/offers/exporting/api?authuser=0
  */
 
-// Exit if accessed directly
 if (!defined('ABSPATH')) {
-    exit;
+    exit; // Exit if accessed directly
 }
 
 class WP_REST_Internships_Controller extends \WP_REST_Controller
@@ -46,30 +45,93 @@ class WP_REST_Internships_Controller extends \WP_REST_Controller
 
         register_rest_route(
             $this->namespace,
-            '/' . $this->rest_base, array(
+            '/' . $this->rest_base . '/domestic', array(
                 array(
                     'methods' => \WP_REST_Server::READABLE,
-                    'callback' => array($this, 'get_internships'),
+                    'callback' => array($this, 'get_foreign_offers'),
+                ),
+            ));
+        register_rest_route(
+            $this->namespace,
+            '/' . $this->rest_base . '/foreign', array(
+                array(
+                    'methods' => \WP_REST_Server::READABLE,
+                    'callback' => array($this, 'get_foreign_offers'),
                 ),
             ));
     }
 
+    public function get_domestic_offers($request)
+    {
+        return $this->get_internships($this->domestic_offers);
+    }
+
+    public function get_foreign_offers($request)
+    {
+        return $this->get_internships($this->foreign_offers);
+    }
+
+    public function get_internships($offer_type)
+    {
+        if (!$this->is_cached($offer_type)) {
+            $internships = $this->fetch_internships($offer_type);
+            $this->write_to_file($internships, $offer_type);
+        }
+        return $this->get_cached_data($offer_type);
+    }
+
+    public function fetch_internships($offer_type)
+    {
+        $url = $this->create_url($offer_type);
+        $request = new WP_Http;
+        $result = $request->request($url);
+        $json = json_decode($result['body'], true);
+        return $json['records'];
+    }
+
     public function create_url($report_id)
     {
-        $username = $this->username;
+        $username = rawurlencode($this->username);
         $password = $this->password;
         $token = $this->token;
         $company_id = $this->company_id;
         return "https://iaeste.smartsimple.ie/API/1/report/?username=$username&password=$password&apitoken=$token&companyid=$company_id&reportid=$report_id";
     }
 
-    public function get_internships($request)
+    public function get_filename($offer_type)
     {
-        $url = $this->create_url($this->foreign_offers);
-        $request = new WP_Http;
-        $result = $request->request($url);
-        $json = json_decode($result['body'], true);
-        return $json['records'];
+        $path = plugin_dir_path(__FILE__);
+        $file_type = ($offer_type == $this->domestic_offers) ? 'domestic' : 'foreign';
+        return $path . '/' . $file_type . '.json';
+    }
+
+    public function write_to_file($data, $offer_type)
+    {
+        $file = $this->get_filename($offer_type);
+        $open = fopen($file, 'w');
+        $write = fputs($open, json_encode($data));
+        fclose($open);
+    }
+
+    public function is_cached($offer_type)
+    {
+        $filename = $this->get_filename($offer_type);
+        if (file_exists($filename)) {
+            $now = new DateTime("now", new DateTimeZone('UTC'));
+            $last_edit = new DateTime(date("F d Y H:i:s.", filemtime($filename)), new DateTimeZone('UTC'));
+            $interval = $last_edit->diff($now);
+            $hour_difference = ($interval->days * 24) + $interval->h;
+            if ($hour_difference < 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function get_cached_data($offer_type)
+    {
+        $file = $this->get_filename($offer_type);
+        return json_decode(file_get_contents($file), true);
     }
 
     public function hook_rest_server()
